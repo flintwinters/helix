eaff8fa4e22d3530b560b4ed851c85e7aec77840
flintwinters <flintwinters.0@gmail.com>
2026-04-11 10:06:47 -0400

    Root runtime objects directly under zygote bindings

--- src/helix.cpp
+++ src/helix.cpp
@@ -11,26 +11,24 @@ Cell& multiply(Cell& c);
 Cell& divide(Cell& c);
 Cell& if_builtin(Cell& c);
 
-Cell& Null = *new AnyCell();
-Cell& Zygote = *new MapCell();
+AnyCell NullStorage;
+MapCell ZygoteStorage;
+
+Cell& Null = NullStorage;
+Cell& Zygote = ZygoteStorage;
 
 void initialize_zygote() {
-    auto* bindings = Zygote.map_value();
-
-    Cell& errors = register_success(new VecCell());
-    (*bindings)["errors"] = &errors;
-
-    (*bindings)["printout"] = &register_success(new FunCell(printout));
-    (*bindings)["+"] = &register_success(new FunCell(add));
-    (*bindings)["-"] = &register_success(new FunCell(subtract));
-    (*bindings)["*"] = &register_success(new FunCell(multiply));
-    (*bindings)["/"] = &register_success(new FunCell(divide));
-    (*bindings)["if"] = &register_success(new FunCell(if_builtin));
+    Zygote.bind("errors", new VecCell());
+    Zygote.bind("printout", new FunCell(printout));
+    Zygote.bind("+", new FunCell(add));
+    Zygote.bind("-", new FunCell(subtract));
+    Zygote.bind("*", new FunCell(multiply));
+    Zygote.bind("/", new FunCell(divide));
+    Zygote.bind("if", new FunCell(if_builtin));
 }
 
 void shutdown_zygote() {
     clear_rooted_errors();
-    clear_success_cells();
 }
 
 int main(int argc, char** argv) {
@@ -43,7 +41,6 @@ int main(int argc, char** argv) {
 
     const string yaml = load_file(argv[1]);
     Cell& parsed = parse_yaml_to_cells(yaml, Zygote);
-    (*Zygote.map_value())["main"] = &parsed;
     cout << cell_to_yaml_string(parsed);
 
     Cell& er = Zygote(Null);
